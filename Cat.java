80734bde5a356c5c6f04962d19255798185f6ec8
ArkadiyAlekseev <89015307380@mail.ru>
2018-10-15 02:06:17 +0300

    выполнил ДЗ к 5 уроку

---
+++ Cat.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.J1Homework5;
+
+public class Cat extends Animal {
+    public Cat(String name, int age) {
+        super(name, age, 200, 2, 0, "Кошка");
+    }
+}
---
+++ Dog.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.J1Homework5;
+
+public class Dog extends Animal {
+    public Dog(String name, int age) {
+        super(name, age, 500, 0.5, 10, "Собака");
+    }
+}
